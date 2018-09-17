aa6670830cfb7a863e1a4e7143adb5edd8933801
hubert.lenoir <hubert.lenoir@sensiolabs.com>
2018-09-17 13:58:47 +0200

    [#1] Concat live videos

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const serveStatic = require('serve-static');
+const videoStitch = require('video-stitch');
+const fs = require('fs');
 
 const SCHEME = 'http';
 const HOST = 'localhost';
 const PORT = 1664;
+const DEBUG = process.NODE_ENV !== 'prod';
 
 // Server
 const app = express();
@@ -17,9 +20,28 @@ app.get('/', function(req, res) {
 
 // Replay
 app.get('/replay/:last', function(req, res) {
-  const last = req.params.last || 5;
+  const last = req.params.last;
+  const videos = fs.readdirSync(__dirname + '/public/video/live');
+  const filename = 'video/replay/'+(new Date()).getTime()+'.mp4';
 
-  res.send(last);
+  videoStitch
+    .concat({
+      silent: !DEBUG,
+      overwrite: true,
+    })
+    .clips(
+      videos
+        .slice(videos.length - last)
+        .map(function(file) {
+          return { fileName: `${__dirname}/public/video/live/${file}` };
+        })
+    )
+    .output(__dirname+'/public/'+filename)
+    .concat()
+    .then(function() {
+      res.send(filename);
+    })
+  ;
 });
 
 // Public
